e61dc0e08cfa66c49a75289e497f636cf98fbab9
Abdosh022 <86729100+Abdosh022@users.noreply.github.com>
2024-04-26 04:29:05 +0300

    database

---
+++ HTI.Core/Entities/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTI.Core.Entities
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseCode { get; set; }
+        public string Name { get; set; }
+        public int Credits { get; set; }
+        public string Description { get; set; }
+        public string Links { get; set; }
+        public int StudyYear { get; set; }
+        public int Semester { get; set; }
+        public int? PrerequisiteId { get; set; }
+
+        public Course Prerequisite { get; set; }
+        public ICollection<Group> Groups { get; set; }
+        public ICollection<StudentCourseHistory> StudentCourseHistories { get; set; }
+    }
+
+}
---
+++ HTI.Core/Entities/TeachingAssistant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HTI.Core.Entities
+{
+    public class TeachingAssistant
+    {
+        public int TeachingAssistantId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Bio { get; set; }
+        public string Cv { get; set; }
+
+        public ICollection<Group> Groups { get; set; }
+        public ICollection<StudentCourseHistory> StudentCourseHistories { get; set; }
+    }
+
+}
