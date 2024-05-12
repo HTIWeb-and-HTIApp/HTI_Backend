f38287533fd318fc25734f62c9f52f301476934b
Abdosh022 <86729100+Abdosh022@users.noreply.github.com>
2024-05-13 02:36:38 +0300

    sdfsf

---
+++ HTI_Backend/Controllers/AnnouncementsController .cs
@@ -0,0 +1,35 @@
+﻿using Announcement;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace HTI_Backend.Controllers
+{
+  
+    public class AnnouncementsController : ApiBaseController
+
+    {
+        private readonly IHubContext<NotificationHub> _notificationHub;
+
+        public AnnouncementsController(IHubContext<NotificationHub> notificationHub )
+        {
+            _notificationHub = notificationHub;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAnnouncement(Announcement__ announcement)
+        {
+        
+
+            await _notificationHub.Clients.All.SendAsync("NewAnnouncement", announcement);
+
+            return CreatedAtAction(nameof(CreateAnnouncement), new { id = announcement.Id }, announcement);
+        }
+
+        // ... other actions (GET, PUT, etc.)
+    }
+
+
+   
+    
+}
+
